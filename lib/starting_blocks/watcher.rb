3baec818a297e2ea6fd83f1aab4d2239203e39e6
Darren Cauthon <darren@cauthon.com>
2013-04-27 09:48:55 -0500

    Rename variables for readability.

--- lib/starting_blocks/watcher.rb
+++ lib/starting_blocks/watcher.rb
@@ -8,38 +8,38 @@ module StartingBlocks
     class << self
       def start_watching(dir, options)
         location = dir.getwd
-        files = Dir['**/*']
+        all_files = Dir['**/*']
         FSSM.monitor(location, '**/*') do
-          update {|base, relative| StartingBlocks::Watcher.run_it relative, files, options }
-          delete {|base, relative| StartingBlocks::Watcher.delete_it relative, files, options }
-          create {|base, relative| StartingBlocks::Watcher.add_it relative, files, options }
+          update {|base, file_that_changed| StartingBlocks::Watcher.run_it file_that_changed, all_files, options }
+          delete {|base, file_that_changed| StartingBlocks::Watcher.delete_it file_that_changed, all_files, options }
+          create {|base, file_that_changed| StartingBlocks::Watcher.add_it file_that_changed, all_files, options }
         end
       end
 
-      def add_it(file, files, options)
-        return if file.index('.git') == 0
-        display "Adding: #{file}"
-        files << file
+      def add_it(file_that_changed, all_files, options)
+        return if file_that_changed.index('.git') == 0
+        display "Adding: #{file_that_changed}"
+        all_files << file_that_changed
       end
 
-      def run_it(file, files, options)
-        specs = get_the_specs_to_run file, files
+      def run_it(file_that_changed, all_files, options)
+        specs = get_the_specs_to_run file_that_changed, all_files
         display "Matches: #{specs.inspect}"
         StartingBlocks::Runner.new(options).run_files specs
       end
 
-      def delete_it(file, files, options)
-        return if file.index('.git') == 0
-        display "Deleting: #{file}"
-        files.delete(file)
+      def delete_it(file_that_changed, all_files, options)
+        return if file_that_changed.index('.git') == 0
+        display "Deleting: #{file_that_changed}"
+        all_files.delete(file_that_changed)
       end
 
       private
 
-      def get_the_specs_to_run(file, files)
-        filename = file.downcase.split('/')[-1].gsub('_spec', '')
-        matches = files.select { |x| x.gsub('_spec.rb', '.rb').include?(filename) && x != file }
-        matches << file
+      def get_the_specs_to_run(file_that_changed, all_files)
+        filename = file_that_changed.downcase.split('/')[-1].gsub('_spec', '')
+        matches = all_files.select { |x| x.gsub('_spec.rb', '.rb').include?(filename) && x != file_that_changed }
+        matches << file_that_changed
         specs = matches.select { |x| x.include?('_spec') && File.file?(x) }.map { |x| File.expand_path x }
       end
     end
