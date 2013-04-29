0c9c66a0aa546a3a33db9e7a7f58d39232daa0d1
Darren Cauthon <darren@cauthon.com>
2013-04-29 18:50:56 -0500

    Refactor.

--- lib/extensions/blinky.rb
+++ lib/extensions/blinky.rb
@@ -7,21 +7,32 @@ module StartingBlocks
       def receive_specs_to_run specs
         @spec_count = specs.count
         return if specs.count == 0
-        Blinky.new.light.building!
+        change_color_to :yellow
       end
 
       def receive_results results
         return if @spec_count == 0
         if (results[:tests] || 0) == 0
-          Blinky.new.light.failure!
+          change_color_to :red
         elsif (results[:errors] || 0) > 0
-          Blinky.new.light.failure!
+          change_color_to :red
         elsif (results[:failures] || 0) > 0
-          Blinky.new.light.failure!
+          change_color_to :red
         elsif (results[:skips] || 0) > 0
-          Blinky.new.light.building!
+          change_color_to :yellow
         else
+          change_color_to :green
+        end
+      end
+
+      def change_color_to(color)
+        case color
+        when :green
           Blinky.new.light.success!
+        when :red
+          Blinky.new.light.failure!
+        when :yellow
+          Blinky.new.light.building!
         end
       end
     end
