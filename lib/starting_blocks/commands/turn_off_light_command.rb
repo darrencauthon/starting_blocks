6ef621d3f1dfcaada57b2d0799db18eee5737304
Darren Cauthon <darren@cauthon.com>
2013-05-20 20:50:42 -0500

    Turn off light when option set.

--- lib/starting_blocks/commands/turn_off_light_command.rb
+++ lib/starting_blocks/commands/turn_off_light_command.rb
@@ -1,5 +1,10 @@
 module StartingBlocks
   class TurnOffLightCommand < Command
+
+    def valid?
+      options[:turn_light_off]
+    end
+
     def execute
       StartingBlocks::Extensions::BlinkyLighting.turn_off!
     end
--- spec/starting_blocks/commands/turn_off_light_command_spec.rb
+++ spec/starting_blocks/commands/turn_off_light_command_spec.rb
@@ -5,6 +5,20 @@ describe StartingBlocks::TurnOffLightCommand do
     StartingBlocks::TurnOffLightCommand.new(nil).is_a? StartingBlocks::Command
   end
 
+  describe "valid?" do
+
+    it "should be true when options include turn_light_off" do
+      StartingBlocks::TurnOffLightCommand.new( { turn_light_off: true } ).
+        valid?.must_equal true
+    end
+
+    it "should be false when options include turn_light_off" do
+      StartingBlocks::TurnOffLightCommand.new( { turn_light_off: false } ).
+        valid?.must_equal false
+    end
+
+  end
+
   describe "execute" do
     it "should call the command to turn off the light" do
       StartingBlocks::Extensions::BlinkyLighting.expects(:turn_off!)
