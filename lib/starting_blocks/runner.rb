edeab3a7367762897cfb10927b1a8aee88fb90fa
Darren Cauthon <darren@cauthon.com>
2013-07-09 16:18:28 -0500

    Allow the no-vendor to be an option passed through command line.

--- lib/starting_blocks/runner.rb
+++ lib/starting_blocks/runner.rb
@@ -6,11 +6,12 @@ module StartingBlocks
     def initialize options
       @verbose = options[:verbose]
       @use_bundler = options[:use_bundler]
+      @include_vendor = options[:no_vendor] != true
     end
 
     def run_files files
       display "Files to run: #{files.inspect}"
-      files = files.select { |x| x.include?('/vendor/') == false }
+      files = files.select { |x| @include_vendor || x.include?('/vendor/') == false }
       StartingBlocks::Publisher.publish_files_to_run files
       results = execute_these_files files
       StartingBlocks::Publisher.publish_results results
