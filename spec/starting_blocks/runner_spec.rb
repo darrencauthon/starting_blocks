358fcc54ea43e12b146cb74222c8e059655b6bb9
Darren Cauthon <darren@cauthon.com>
2013-07-24 22:59:38 -0500

    Add a test for multiple files.

--- spec/starting_blocks/runner_spec.rb
+++ spec/starting_blocks/runner_spec.rb
@@ -38,4 +38,39 @@ describe StartingBlocks::Runner do
       @results.must_be_same_as results
     end
   end
+
+  describe "run two files, no exceptions" do
+    let(:files)   { ['test1.rb', 'test2.rb'] }
+    let(:results) { Object.new }
+
+    before do
+      runner.expects(:display).with('Files to run: ["test1.rb", "test2.rb"]')
+      runner.expects(:execute_these_files).with(files).returns results
+      StartingBlocks::Publisher.expects(:publish_files_to_run).with files
+      StartingBlocks::Publisher.expects(:publish_results).with results
+      runner.expects(:puts).with results
+
+      @results = runner.run_files files
+    end
+
+    it "should display the files" do
+      # expectation set above
+    end
+
+    it "should public the files to run" do
+      # expectation set above
+    end
+
+    it "should public the results" do
+      # expectation set above
+    end
+
+    it "should put the results" do
+      # expectation set above
+    end
+
+    it "should return the reuslts" do
+      @results.must_be_same_as results
+    end
+  end
 end
