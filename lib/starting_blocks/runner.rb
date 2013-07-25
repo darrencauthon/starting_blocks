8e4aef0b6b8ea4525267d0077001b077c3fefaf4
Darren Cauthon <darren@cauthon.com>
2013-07-24 23:07:02 -0500

    Add test for excluding vendor.

--- lib/starting_blocks/runner.rb
+++ lib/starting_blocks/runner.rb
@@ -4,14 +4,13 @@ module StartingBlocks
     include Displayable
 
     def initialize options
-      @verbose = options[:verbose]
       @use_bundler = options[:use_bundler]
       @include_vendor = options[:no_vendor] != true
     end
 
     def run_files files
-      display "Files to run: #{files.inspect}"
       files = files.select { |x| @include_vendor || x.include?('/vendor/') == false }
+      display "Files to run: #{files.inspect}"
       StartingBlocks::Publisher.publish_files_to_run files
       results = execute_these_files files
       StartingBlocks::Publisher.publish_results results
--- spec/starting_blocks/runner_spec.rb
+++ spec/starting_blocks/runner_spec.rb
@@ -73,4 +73,42 @@ describe StartingBlocks::Runner do
       @results.must_be_same_as results
     end
   end
+
+  describe "run two files, one from vendor, but no vendor allowed!" do
+    let(:files)   { ['test1.rb', '/vendor/something_test.rb', 'test2.rb'] }
+    let(:options) { { no_vendor: true } }
+    let(:results) { Object.new }
+
+    let(:files_without_vendor) { ['test1.rb', 'test2.rb'] }
+
+    before do
+      runner.expects(:display).with('Files to run: ["test1.rb", "test2.rb"]')
+      runner.expects(:execute_these_files).with(files_without_vendor).returns results
+      StartingBlocks::Publisher.expects(:publish_files_to_run).with files_without_vendor
+      StartingBlocks::Publisher.expects(:publish_results).with results
+      runner.expects(:puts).with results
+
+      @results = runner.run_files files
+    end
+
+    it "should display the files" do
+      # expectation set above
+    end
+
+    it "should public the files to run" do
+      # expectation set above
+    end
+
+    it "should public the results" do
+      # expectation set above
+    end
+
+    it "should put the results" do
+      # expectation set above
+    end
+
+    it "should return the reuslts" do
+      @results.must_be_same_as results
+    end
+  end
 end
