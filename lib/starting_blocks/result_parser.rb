92f4f92bc62690f23120e5f259503582716cc338
Darren Cauthon <darren@cauthon.com>
2014-02-20 23:49:03 -0600

    Refactor.

--- lib/starting_blocks/result_parser.rb
+++ lib/starting_blocks/result_parser.rb
@@ -1,19 +1,35 @@
 module StartingBlocks
   class ResultParser
+
     def parse text
-      output = StartingBlocks::ResultTextParser.new.parse text
-      if output[:tests] == 0
-        output[:color] = :red
-      elsif (output[:errors] || 0) > 0
-        output[:color] = :red
-      elsif (output[:failures] || 0) > 0
-        output[:color] = :red
-      elsif (output[:skips] || 0) == 0
-        output[:color] = :green
-      else
-        output[:color] = :yellow
-      end
+      output = load_the_output_from text
+      output[:color] = color
       output
+    end
+
+    private
+
+    def color
+      return :red unless tests_exist?
+      return :red if problems_exist?
+      return :yellow if skips_exist?
+      :green
+    end
+
+    def load_the_output_from text
+      @output = StartingBlocks::ResultTextParser.new.parse text
+    end
+
+    def tests_exist?
+      (@output[:tests] || 0) > 0
+    end
+
+    def problems_exist?
+      ((@output[:errors] || 0) > 0) or ((@output[:failures] || 0) > 0)
+    end
+
+    def skips_exist?
+      (@output[:skips] || 0) > 0
     end
   end
 end
