8cc65c9da0ec0c63c4a80c5e524994877a4ceb30
Darren Cauthon <darren@cauthon.com>
2014-03-05 21:39:00 -0600

    Move the CLI stuff into a single method.

---
+++ lib/starting_blocks/cli.rb
@@ -0,0 +1,58 @@
+module StartingBlocks
+  module Cli
+    def self.run
+      arguments = [ARGV, (File.exists?(File.expand_path('~/.sb')) ? File.read(File.expand_path('~/.sb')).split(' ') : [])].flatten
+
+      [:blinky, :growl, :stopplicht].each { |p| require "starting_blocks-#{p}" if arguments.include? "--#{p}" }
+
+      StartingBlocks.verbose = arguments.include? '--verbose'
+
+      options = {
+                  no_vendor:   (arguments.include?('--vendor') == false),
+                  use_bundler: (Dir['Gemfile'].count > 0)
+                }
+
+      run_all_specs = ->(options) do
+                           files = ['**/*_spec.rb*', '**/*_test.rb*', '**/test_*.rb*'].map do |d|
+                             Dir[d].
+                               select { |f| File.file?(f) }.
+                               map    { |x| File.expand_path(x) }
+                           end.flatten
+
+                           StartingBlocks::Runner.new(options).run_files files
+                         end
+
+      actions = {
+                  watch: -> do
+                              listener = StartingBlocks::Watcher.start_watching Dir, options
+                              StartingBlocks.display "Going to sleep, waiting for changes"
+
+                              puts 'Enter "stop" to stop the listener'
+
+                              listener.start
+                              loop do
+                                user_input = STDIN.gets
+                                if user_input == "stop\n"
+                                  exit
+                                end
+                              end
+                            end,
+                  run_all_tests: -> do
+                                      results = run_all_specs.call(options)
+
+                                      parsed_results = StartingBlocks::Publisher.result_parser.parse(results)
+                                      success = parsed_results[:color] == :green
+
+                                      exit success
+                                    end,
+                  off: -> do
+                            StartingBlocks::Extensions::BlinkyLighting.turn_off!
+                          end
+                }
+
+      name_of_action_to_take = [:watch, :off].select { |x| arguments.include? "--#{x}" }.first || :run_all_tests
+
+      actions[name_of_action_to_take].call
+    end
+  end
+end
