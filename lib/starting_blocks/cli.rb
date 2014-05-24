fbbc48b19a279132e8a3efc5045132eb6ce02634
Darren Cauthon <darren@cauthon.com>
2014-05-24 17:43:36 -0500

    More refactors.

--- lib/starting_blocks/cli.rb
+++ lib/starting_blocks/cli.rb
@@ -8,55 +8,54 @@ module StartingBlocks
 
       operations_to_always_run.each { |_, o| o.call }
 
-      run_all_specs = ->() do
-                           files = ['**/*_spec.rb*', '**/*_test.rb*', '**/test_*.rb*'].map do |d|
-                             Dir[d].
-                               select { |f| File.file?(f) }.
-                               map    { |x| File.expand_path(x) }
-                           end.flatten
-
-                           StartingBlocks::Runner.new(StartingBlocks.options).run_files files
-                         end
-
-      actions = {
-                  watch: -> do
-                              listener = StartingBlocks::Watcher.start_watching Dir, StartingBlocks.options
-                              StartingBlocks.display "Going to sleep, waiting for changes"
-
-                              puts 'Enter "stop" to stop the listener'
-                              puts 'Enter a blank line to run all of the tests'
-
-                              listener.start
-                              loop do
-                                user_input = STDIN.gets
-                                if user_input == "stop\n"
-                                  exit
-                                elsif user_input == "\n"
-                                  run_all_specs.call
-                                end
-                              end
-                            end,
-                  run_all_tests: -> do
-                                      results = run_all_specs.call
-
-                                      parsed_results = StartingBlocks::Publisher.result_parser.parse(results)
-                                      success = parsed_results[:color] == :green
-
-                                      exit success
-                                    end,
-                  off: -> do
-                            StartingBlocks::Extensions::BlinkyLighting.turn_off!
-                          end
-                }
-
       name_of_action_to_take = [:watch, :off].select { |x| StartingBlocks.arguments.include? x }.first || :run_all_tests
 
       actions[name_of_action_to_take].call
     end
 
+    def self.run_all_specs
+      ->() do
+           files = ['**/*_spec.rb*', '**/*_test.rb*', '**/test_*.rb*'].map do |d|
+             Dir[d].
+               select { |f| File.file?(f) }.
+               map    { |x| File.expand_path(x) }
+           end.flatten
+                           StartingBlocks::Runner.new(StartingBlocks.options).run_files files
+         end
+    end
+
     def self.build_all_arguments_with arguments
       args = [arguments, default_arguments].flatten
       args.map { |x| x.gsub('--', '').to_sym }
+    end
+
+    def self.actions
+      {
+        watch: -> do
+                    listener = StartingBlocks::Watcher.start_watching Dir, StartingBlocks.options
+                    StartingBlocks.display "Going to sleep, waiting for changes"
+                    puts 'Enter "stop" to stop the listener'
+                    puts 'Enter a blank line to run all of the tests'
+                    listener.start
+                    loop do
+                      user_input = STDIN.gets
+                      if user_input == "stop\n"
+                        exit
+                      elsif user_input == "\n"
+                        run_all_specs.call
+                      end
+                    end
+                  end,
+        run_all_tests: -> do
+                            results = run_all_specs.call
+                                      parsed_results = StartingBlocks::Publisher.result_parser.parse(results)
+                            success = parsed_results[:color] == :green
+                                      exit success
+                          end,
+        off: -> do
+                  StartingBlocks::Extensions::BlinkyLighting.turn_off!
+                end
+      }
     end
 
     def self.default_arguments
