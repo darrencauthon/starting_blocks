6aefa1ba18372098335169880d6f4ea63884a4f5
Darren Cauthon <darren@cauthon.com>
2014-06-24 12:16:04 -0500

    Attempt to fix weird issue.

--- lib/starting_blocks/minitest_contract.rb
+++ lib/starting_blocks/minitest_contract.rb
@@ -1,5 +1,40 @@
 module StartingBlocks
 
+  class Contract
+
+    attr_reader :options
+
+    def initialize options
+      @options = options
+    end
+
+    def self.inherited klass
+      @contract_types ||= []
+      @contract_types << klass
+    end
+
+    def file_clues
+      ["test", "spec"]
+    end
+
+    def extensions
+      []
+    end
+
+    def filter_these_files files
+      files
+    end
+
+    def execute_these_files files
+      raise 'You have to define how to execute these files.'
+    end
+
+    def self.for options
+      @contract_types.last.new options
+    end
+
+  end
+
   class MinitestContract < Contract
 
     def file_clues
