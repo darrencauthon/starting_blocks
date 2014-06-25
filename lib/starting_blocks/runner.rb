b1548cd88b0b626a7d03d81d568658f952f83f5d
Darren Cauthon <darren@cauthon.com>
2014-06-25 09:50:38 -0500

    Remove the period in the extension.

--- lib/starting_blocks/runner.rb
+++ lib/starting_blocks/runner.rb
@@ -6,7 +6,8 @@ module StartingBlocks
     end
 
     def run_files files
-      files = files.select { |x| @contract.extensions.include? x.split('/')[-1].split('.')[-1] }
+      extensions = @contract.extensions.map { |x| x.gsub('.', '')
+      files = files.select { |x| extensions.include? x.split('/')[-1].split('.')[-1] }
       files = @contract.filter_these_files files 
       StartingBlocks.display "Files to run: #{files.inspect}"
       StartingBlocks::Publisher.publish_files_to_run files
