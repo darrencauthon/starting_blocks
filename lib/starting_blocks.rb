64eb50f8dbecb7f4dd32092ef76d8d0fc68296cc
Darren Cauthon <darren@cauthon.com>
2014-07-16 22:12:49 -0500

    This file should be referenced like the others.

--- lib/starting_blocks.rb
+++ lib/starting_blocks.rb
@@ -1,4 +1,4 @@
-require "starting_blocks/version"
+require_relative "starting_blocks/version"
 require_relative 'starting_blocks/runner'
 require_relative 'starting_blocks/watcher'
 require_relative 'starting_blocks/result_parser'
