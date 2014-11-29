ab4db7d0f796de8d4b8961852ccbbec0a439aaf0
Darren Cauthon <darren@cauthon.com>
2014-11-29 14:01:54 -0600

    Move the ability to execute a statement to its own class.

--- lib/starting_blocks/default.rb
+++ lib/starting_blocks/default.rb
@@ -16,20 +16,6 @@ module StartingBlocks
 
     def self.actions
       {
-        execute: -> do
-                      StartingBlocks::Publisher.result_builder = StartingBlocks::PassThroughResultBuilder.new
-
-                      statement_to_execute = ARGV[ARGV.index('execute') + 1]
-                      StartingBlocks::Publisher.publish_files_to_run [statement_to_execute]
-                      result = StartingBlocks::Bash.run(statement_to_execute)
-                      StartingBlocks::Publisher.publish_results( { color: (result[:success] ? :green : :red),
-                                                                   tests: 1,
-                                                                   assertions: 1,
-                                                                   failures: (result[:success] ? 0 : 1),
-                                                                   errors: 0,
-                                                                   skips: 0 })
-                      puts result[:text]
-                    end,
         run_all_tests: -> do
                             results = run_all_specs.call
                                       parsed_results = StartingBlocks::Publisher.result_builder.build_from results
