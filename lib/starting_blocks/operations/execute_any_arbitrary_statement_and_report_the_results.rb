8ca3f7ef334be494ae7f1cbf738d348207a5ec13
Darren Cauthon <darren@cauthon.com>
2014-11-30 09:23:45 -0600

    Formatting.

--- lib/starting_blocks/operations/execute_any_arbitrary_statement_and_report_the_results.rb
+++ lib/starting_blocks/operations/execute_any_arbitrary_statement_and_report_the_results.rb
@@ -12,12 +12,12 @@ module StartingBlocks
       statement_to_execute = ARGV[ARGV.index('execute') + 1]
       StartingBlocks::Publisher.publish_files_to_run [statement_to_execute]
       result = StartingBlocks::Bash.run(statement_to_execute)
-      StartingBlocks::Publisher.publish_results( { color: (result[:success] ? :green : :red),
-                                                   tests: 1,
+      StartingBlocks::Publisher.publish_results( { color:      (result[:success] ? :green : :red),
+                                                   tests:      1,
                                                    assertions: 1,
-                                                   failures: (result[:success] ? 0 : 1),
-                                                   errors: 0,
-                                                   skips: 0 })
+                                                   failures:   (result[:success] ? 0 : 1),
+                                                   errors:     0,
+                                                   skips:      0 } )
       puts result[:text]
     end
 
